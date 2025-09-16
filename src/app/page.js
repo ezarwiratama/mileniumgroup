06c0ff65264c1450b90b7def8740d38f925457a7
ezarwiratama <ezar.hardin10@gmail.com>
2025-09-16 10:31:49 +0700

    add routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
 import Hero from "@/components/Hero"
 import Tentang from "@/components/Tentang"
+import Produk from "@/components/Produk";
+import Project from "@/components/Project";
+import Team from "@/components/Team";
+import SubsidiarySection from "@/components/SubsidiarySection";
+import LocationSection from "@/components/LocationSection";
 
 export default function Home() {
   return (
     <>
     <Hero />
     <Tentang />
+    <Produk />
+    <Project />
+    <Team />
+    <SubsidiarySection />
+    <LocationSection />
     </>
   );
 }
